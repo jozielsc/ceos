edb207f04e6921a6b8019c5f57cf333d6bf92897
Joziel da Silva Costa <jozielcosta@mpto.mp.br>
2021-05-27 11:11:21 -0300

    algo

--- app-vol/ceos/app.py
+++ app-vol/ceos/app.py
@@ -2,10 +2,16 @@
 from os import path
 
 from flask import Flask
+# from flask_restx import Api
+#
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-from ceos.db import db, ma
 from ceos.settings import app_config
-from ceos.pseudocrawler.route import app as crawler
+# from ceos.db import db, ma
+# from ceos.pseudocrawler.route import app as crawler
+
+from ceos.resttest.namespace import blueprint as api
+from ceos.lysis.namespace import blueprint as lysis
 
 def create_app(config_name=None, extra_config=None):
 
@@ -15,15 +21,21 @@ def create_app(config_name=None, extra_config=None):
         instance_relative_config=True
     )
 
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('settings.py')
 
     if extra_config:
         app.config.update(**extra_config)
 
-    db.init_app(app)
-    ma.init_app(app)
 
-    app.register_blueprint(crawler, url_prefix='/crawler')
+
+    # db.init_app(app)
+    # ma.init_app(app)
+    #
+    # app.register_blueprint(crawler, url_prefix='/crawler')
+
+    app.register_blueprint(api, url_prefix='/api/v1')
+    app.register_blueprint(lysis, url_prefix='/api/v2')
 
     return app
